c79482d739e660e163a2953855f6d2a88e18595a
syam-ts <syamnandhu3@gmail.com>
2025-06-04 00:45:50 +0530

    fix: navbar bug fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 
 export const config = {
-    BASE_URL: import.meta.env.VITE_FRONTEND_ORIGIN,
+    BASE_URL: import.meta.env.VITE_FRONTEND_ORIGIN || 'https://readora-puce.vercel.app',
     // SERVER_URL: 'http://localhost:3000',
      SERVER_URL: 'https://readora-backend.onrender.com',
     CLOUDINARY_BASEURL: import.meta.env.VITE_CLOUDINARY_BASEURL

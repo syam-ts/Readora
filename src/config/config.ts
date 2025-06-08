122d2a384bcab763068482736a1995ab2416c918
syam-ts <syamnandhu3@gmail.com>
2025-06-08 19:41:31 +0530

    chore: add production url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 
 export const config = {
-    BASE_URL: import.meta.env.VITE_FRONTEND_ORIGIN || 'http://localhost:5173',
-    // BASE_URL: import.meta.env.VITE_FRONTEND_ORIGIN || 'https://readora-puce.vercel.app',
-    //  SERVER_URL: 'https://readora-backend.onrender.com',
-    SERVER_URL: 'http://localhost:3000',
+    // BASE_URL: import.meta.env.VITE_FRONTEND_ORIGIN || 'http://localhost:5173',
+      BASE_URL: import.meta.env.VITE_FRONTEND_ORIGIN || 'https://readora-puce.vercel.app',
+     SERVER_URL: 'https://readora-backend.onrender.com',
+    // SERVER_URL: 'http://localhost:3000',
     CLOUDINARY_BASEURL: import.meta.env.VITE_CLOUDINARY_BASEURL
 }   
